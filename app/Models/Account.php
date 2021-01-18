979b71c85a1f088132628acdc4f71ba5420e5b71
nataprawiraf <nataprawiraf@gmail.com>
2021-01-18 12:13:58 +0700

    finish model and define relationships between models

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Transaction;
 
 class Account extends Model
 {
@@ -15,14 +16,45 @@ class Account extends Model
      * @var array
      */
     protected $fillable = [
-        'name',
-        'symbol',
-        'to_dollar',
     ];
+
+    public function currency()
+    {
+        return $this->belongsTo(Currency::class);
+    }
+
+    public function transactions() 
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function get_total_income()
+    {
+        $transactions = $this
+                            ->transactions()
+                            ->where('type', 'income')
+                            ->get();
+
+        return $transactions->sum(function ($transaction) {
+            return $transaction->amount;
+        });
+ 
+    }
+
+    public function get_total_expense()
+    {
+        $transactions = $this
+                            ->transactions()
+                            ->where('type', 'expense')
+                            ->get();
+
+        return $transactions->sum(function ($transaction) {
+            return $transaction->amount;
+        });
+    }
 
     public function get_total_balance()
     {
-        return 0;
+        return $this->get_total_income() - $this->get_total_expense();
     }
-
 }
