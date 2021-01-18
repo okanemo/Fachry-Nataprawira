d8bcb5daa0685cc1819eb9f534301036ebe218ee
nataprawiraf <nataprawiraf@gmail.com>
2021-01-18 18:43:10 +0700

    correct how to query account from users in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\Account;
 use App\Models\Transaction;
 use App\Models\Category;
+use App\Models\Currency;
 
 use Log;
 
@@ -31,14 +32,16 @@ class HomeController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $accounts = Account::where('id', $user->id)->get();
+        $accounts = $user->accounts;
         $transactions = Transaction::where('user_id', $user->id)->get();
         $categories = Category::all();
+        $currencies = Currency::all();
 
         return view('home')
             ->with(array('accounts'=>$accounts, 
                         'user_id'=>$user->id, 
                         'categories'=>$categories, 
+                        'currencies'=>$currencies, 
                         'transactions'=>$transactions));
     }
 }
