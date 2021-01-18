0c293b4e942b05f703396843dbba17b18e801e57
nataprawiraf <nataprawiraf@gmail.com>
2021-01-18 12:13:41 +0700

    add home controller and view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Models\User;
+use App\Models\Account;
+use App\Models\Transaction;
+
+use Log;
 
 class HomeController extends Controller
 {
@@ -23,6 +29,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        $accounts = Account::where('id', $user->id)->get();
+        $transactions = Transaction::where('user_id', $user->id)->get();
+
+        return view('home')
+            ->with(array('accounts'=>$accounts, 
+                        'user_id'=>$user->id, 
+                        'transactions'=>$transactions));
     }
 }
