f6e81c562272e9de936b712de66bd031787ede65
nataprawiraf <nataprawiraf@gmail.com>
2021-01-18 17:45:08 +0700

    add relationship between models

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -7,10 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Account;
 use App\Models\User;
+use App\Models\SubCategory;
+use App\Models\Category;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'type',
+        'date',
+        'amount',
+        'account_id',
+        'user_id',
+        'sub_category_id'
+    ];
 
     public function account()
     {
@@ -20,5 +32,15 @@ class Transaction extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function sub_category()
+    {
+        return $this->belongsTo(SubCategory::class);
+    }
+
+    public function category()
+    {
+        return $this->sub_category()->category();
     }
 }
