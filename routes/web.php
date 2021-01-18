04df8fef5dc0c02bcd0b037b0ad75739645afcb8
nataprawiraf <nataprawiraf@gmail.com>
2021-01-18 12:14:44 +0700

    add middlewere for verification of user authentication

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group( ['middleware' => 'auth' ], function()
+{
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
